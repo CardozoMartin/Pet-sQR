b827debc54a4bbcc870f56cae6622a1eb7699225
Martin Cardozo <tiin.-@hotmail.es>
2024-02-28 00:00:38 -0300

    se agregan middlewares(validaciones) en el postusers

--- servidor-petQR/src/routes/useRoutes.js
+++ servidor-petQR/src/routes/useRoutes.js
@@ -6,11 +6,13 @@ import {
   postUser,
   putUser,
 } from '../controllers/userController.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { postUserSchema } from '../helpers/validationSchemas/userSchemaValidation.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
-router.post('/', postUser);
+router.post('/', (res, req, next) => validateBody(req, res, next, postUserSchema), postUser);
 router.put('/:id', putUser);
 router.delete('/:id', deleteUser);
 
