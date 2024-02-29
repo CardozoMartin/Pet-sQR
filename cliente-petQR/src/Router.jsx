c3ea815bb28dc795d3bbde7a7ef25b8bb2aca381
Martin Cardozo <tiin.-@hotmail.es>
2024-02-28 23:36:51 -0300

    se crea estructura principal del front

---
+++ cliente-petQR/src/Router.jsx
@@ -0,0 +1,24 @@
+import {  Routes } from "react-router"
+import { BrowserRouter } from "react-router-dom"
+
+
+const Router = () => {
+  return (
+    <>
+     <BrowserRouter>
+        
+      <main className="mt-4">
+
+        <Routes>
+          {/*<Route exact path="/" element={} />*/ }
+          
+
+        </Routes>
+      </main>
+      
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default Router
--- cliente-petQR/src/main.jsx
+++ cliente-petQR/src/main.jsx
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-import App from './App.jsx'
+import Router from './Router.jsx';
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
 )
