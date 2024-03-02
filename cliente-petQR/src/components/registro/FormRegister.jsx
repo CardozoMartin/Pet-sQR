5c861e67b6619b8ffb68bafe109c11fcf095da41
martin <tiin.-@hotmail.es>
2024-03-02 15:28:20 -0300

    se crea estrucutara y se obtiene la informacion desde el formulario

---
+++ cliente-petQR/src/components/registro/FormRegister.jsx
@@ -0,0 +1,128 @@
+import { useForm } from "react-hook-form";
+import Input from "../input/Input";
+
+const FormRegister = () => {
+  const { register, handleSubmit: onSubmitRHF } = useForm();
+
+  const handleSubmit = (data) => {
+    //agregar spinner
+    console.log(data);
+  };
+  return (
+    <>
+      <form
+        action="#"
+        className="mt-8 grid grid-cols-6 gap-6"
+        onSubmit={onSubmitRHF(handleSubmit)}
+      >
+        <Input
+          register={register}
+          placeholder="Nombre"
+          type="text"
+          className="col-span-6 sm:col-span-3"
+          options={{
+            required: true,
+            minLength: 4,
+            maxLength: 6000,
+          }}
+          name="Nombre"
+        />
+
+        <Input
+          register={register}
+          placeholder="Apellido"
+          type="text"
+          className="col-span-6 sm:col-span-3"
+          options={{
+            required: true,
+            minLength: 4,
+            maxLength: 6000,
+          }}
+          name="Apellido"
+        />
+
+        <Input
+          register={register}
+          placeholder="Email"
+          type="email"
+          className="col-span-6"
+          options={{
+            required: true,
+            minLength: 4,
+            maxLength: 6000,
+          }}
+          name="Email"
+        />
+        <Input
+          register={register}
+          placeholder="Direccion"
+          type="text"
+          className="col-span-6 sm:col-span-3"
+          options={{
+            required: true,
+            minLength: 4,
+            maxLength: 6000,
+          }}
+          name="Direccion"
+        />
+        <Input
+          register={register}
+          placeholder="Telefono"
+          type="text"
+          className="col-span-6 sm:col-span-3"
+          options={{
+            required: true,
+            minLength: 4,
+            maxLength: 6000,
+          }}
+          name="Telefono"
+        />
+
+        <Input
+          register={register}
+          placeholder="Contraseña"
+          type="password"
+          className="col-span-6 sm:col-span-3"
+          options={{
+            required: true,
+            minLength: 4,
+            maxLength: 6000,
+          }}
+          name="Contraseña"
+        />
+
+        <fieldset className="col-span-6">
+          <label htmlFor="MarketingAccept" className="flex gap-4">
+            <input
+              type="checkbox"
+              id="MarketingAccept"
+              name="marketing_accept"
+              className="size-5 rounded-md border-gray-200 bg-white shadow-sm"
+            />
+
+            <span className="text-sm text-gray-700">
+              Deseo Recibir informacion sobre actualizacion en mi email
+            </span>
+          </label>
+        </fieldset>
+
+        <fieldset className="col-span-6 sm:flex sm:items-center sm:gap-4">
+          <button className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-blue-900 hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
+            Crear cuenta
+          </button>
+
+          <p className="mt-4 text-sm text-gray-500 sm:mt-0">
+            Ya tienes cuenta?
+            <button className="text-gray-700 underline font-bold ">
+              {" "}
+              Iniciar
+            </button>
+            .
+          </p>
+        </fieldset>
+      </form>
+    </>
+  );
+};
+
+export default FormRegister;
