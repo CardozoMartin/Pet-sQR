1c42444a91cfd81afd9c501bfff9b162b6d56f78
martin <tiin.-@hotmail.es>
2024-03-24 23:31:14 -0300

    se agregan animaciones al home

--- cliente-petQR/src/views/InicioView.jsx
+++ cliente-petQR/src/views/InicioView.jsx
@@ -1,11 +1,19 @@
-
 import { Link } from "react-router-dom";
 import Answer from "../components/home/Answer";
 import Contacto from "../components/home/Contacto";
 
-const InicioView = () => {
-  
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
+const InicioView = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 800, // duración de las animaciones en ms
+      easing: 'ease', // función de tiempo para la animación
+      once: true // si se debe ejecutar la animación solo una vez
+    });
+  }, []);
   
   return (
     <>
@@ -14,15 +22,15 @@ const InicioView = () => {
           <div className="-mx-4 flex flex-wrap items-center justify-between">
             <div className="w-full px-4 lg:w-6/12">
               <div className="-mx-3 flex items-center sm:-mx-4">
-                <div className="w-full px-3 sm:px-4 xl:w-1/2">
-                  <div className="py-3 sm:py-4">
+                <div className="w-full px-3 sm:px-4 xl:w-1/2" data-aos="fade-up">
+                  <div className="py-3 sm:py-4" data-aos="fade-up">
                     <img
                       src="https://i.pinimg.com/736x/5c/e4/5d/5ce45d6908a00a6899d7c62c0739db70.jpg"
                       alt=""
                       className="w-full rounded-2xl"
                     />
                   </div>
-                  <div className="py-3 sm:py-4">
+                  <div className="py-3 sm:py-4 " data-aos="fade-up">
                     <img
                       src="https://media.istockphoto.com/id/1483360428/es/foto/lindo-gato-atigrado-joven-jugando-en-un-jard%C3%ADn.webp?b=1&s=170667a&w=0&k=20&c=BLWIVwiiOTwqs_mz5bqP4J3XK9ngto68FJKOj5Je5yo="
                       alt=""
@@ -30,7 +38,7 @@ const InicioView = () => {
                     />
                   </div>
                 </div>
-                <div className="w-full px-3 sm:px-4 xl:w-1/2">
+                <div className="w-full px-3 sm:px-4 xl:w-1/2" data-aos="fade-left">
                   <div className="relative z-10 my-4">
                     <img
                       src="https://images.unsplash.com/photo-1552053831-71594a27632d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBlcnJvJTIwaGQlMjB3YWxscGFwZXJxfGVufDB8fDB8fHww"
@@ -613,131 +621,133 @@ const InicioView = () => {
             </div>
             <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
               <div className="mt-10 lg:mt-0">
-                <span className="mb-4 block text-lg font-semibold text-primary">
+                <h1 className="mb-4 block text-lg font-semibold  fs-1 text-orange-600">
                   Bienvenido a PetsQR
-                </span>
-                <h2 className="mb-5 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
-                Una comunidad unida por el amor a las mascotas: Registra la tuya hoy
+                </h1>
+                <h2 className="mb-5 text-3xl font-bold text-dark dark:text-white sm:text-[20px]/[48px]">
+                  Una comunidad unida por el amor a las mascotas: Registra la
+                  tuya hoy
                 </h2>
-                <p className="mb-5 text-base text-body-color dark:text-dark-6">
-                  Registrando a tus mascotas generas un Qr o un PetID donde pondras todos tus datos para contactarte en caso de que tu mascota este extraviada.
+                <p className="mb-5 text-base text-body-color dark:text-dark-6 font-semibold">
+                  Registrando a tus mascotas generas un Qr o un PetID donde
+                  pondras todos tus datos para contactarte en caso de que tu
+                  mascota este extraviada.
                 </p>
-                <p className="mb-8 text-base text-body-color dark:text-dark-6">
+                <p className="mb-8 text-base text-body-color dark:text-dark-6 font-semibold">
                   Comienza ahora registrandote y cargando a tus mascotas ♥
                 </p>
-                <Link to="/registro"
-                  
-                  className="inline-flex items-center justify-center rounded-md border border-transparent bg-primary px-7 py-3 text-center text-base font-medium text-white hover:bg-opacity-90"
-                >
-                  Registrate
-                </Link>
+                <div className="text-center" data-aos="fade-up">
+                  <Link
+                    to="/registro"
+                    className="inline-flex items-center justify-center rounded-md border border-transparent bg-orange-500 px-5 py-3 text-center text-base font-medium text-white hover:bg-opacity-90"
+                  >
+                    Registrate
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </section>
       <section className="bg-white pb-10  dark:bg-dark lg:pb-20 container">
-      <div className="container mx-auto">
-        <div className="-mx-4 flex flex-wrap justify-center">
-          <div className="w-full px-4 container">
-            <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
-              <span className="mb-2 block text-lg font-semibold text-primary">
-                PetsQR
-              </span>
-             
-              <p className="text-base text-body-color dark:text-dark-6">
-                Todo lo que te ofrecemos para ti y tu mascotas
-              </p>
+        <div className="container mx-auto">
+          <div className="-mx-4 flex flex-wrap justify-center">
+            <div className="w-full px-4 container">
+              <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
+                <span className="mb-2 block text-lg font-semibold text-primary">
+                  PetsQR
+                </span>
+
+                <p className="text-base text-body-color dark:text-dark-6">
+                  Todo lo que te ofrecemos para ti y tu mascotas
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+              <div className="mb-10 w-full">
+                <div className="mb-8 overflow-hidden rounded">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFgev8h47A1lbQ6hbZ8alDIA0JwhVEb1qHcs_lgtzv0w&s"
+                    alt="image"
+                    className="w-full"
+                  />
+                </div>
+                <div>
+                  <h3>
+                    <a
+                      href="javascript:void(0)"
+                      className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
+                    >
+                      Podras agregar todas las mascotas que quieras
+                    </a>
+                  </h3>
+                  <p className="text-base text-body-color dark:text-dark-6">
+                    con toda su informacion personal y cuidados medicos en caso
+                    de necesitarlos.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+              <div className="mb-10 w-full">
+                <div className="mb-8 overflow-hidden rounded">
+                  <img
+                    src="https://www.escuelamonicagomez.com/archivos/image/_noticias/collares/collar-de-nylon.jpg"
+                    alt="image"
+                    className="w-full"
+                  />
+                </div>
+                <div>
+                  <h3>
+                    <a
+                      href="javascript:void(0)"
+                      className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
+                    >
+                      Podras generar un codigo QR o PetID
+                    </a>
+                  </h3>
+                  <p className="text-base text-body-color dark:text-dark-6">
+                    con ambos podras acceder a toda tu informacion de contacto
+                    !!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="w-80 px-4 md:w-1/2 lg:w-1/3">
+              <div className="mb-10 w-full">
+                <div className="mb-8 overflow-hidden rounded">
+                  <img
+                    src="https://www.diarioveterinario.com/images/showid2/3201219?w=900"
+                    alt="image"
+                    className="w-full"
+                  />
+                </div>
+                <div>
+                  <h3>
+                    <a
+                      href="javascript:void(0)"
+                      className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
+                    >
+                      Podras modificar toda su informacion
+                    </a>
+                  </h3>
+                  <p className="text-base text-body-color dark:text-dark-6">
+                    podras editar todos los toda la informacion de contacto
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+      </section>
 
-        <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-            <div className="mb-10 w-full">
-              <div className="mb-8 overflow-hidden rounded">
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFgev8h47A1lbQ6hbZ8alDIA0JwhVEb1qHcs_lgtzv0w&s"
-                  alt="image"
-                  className="w-full"
-                />
-              </div>
-              <div>
-                
-                <h3>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
-                  >
-                    Podras agregar todas las mascotas que quieras
-                  </a>
-                </h3>
-                <p className="text-base text-body-color dark:text-dark-6">
-                  con toda su informacion personal y cuidados medicos en caso de necesitarlos.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-            <div className="mb-10 w-full">
-              <div className="mb-8 overflow-hidden rounded">
-                <img
-                  src="https://www.escuelamonicagomez.com/archivos/image/_noticias/collares/collar-de-nylon.jpg"
-                  alt="image"
-                  className="w-full"
-                />
-              </div>
-              <div>
-               
-                <h3>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
-                  >
-                    Podras generar un codigo QR o PetID
-                  </a>
-                </h3>
-                <p className="text-base text-body-color dark:text-dark-6">
-                  con ambos podras acceder a toda tu informacion de contacto !!
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-80 px-4 md:w-1/2 lg:w-1/3">
-            <div className="mb-10 w-full">
-              <div className="mb-8 overflow-hidden rounded">
-                <img
-                  src="https://www.diarioveterinario.com/images/showid2/3201219?w=900"
-                  alt="image"
-                  className="w-full"
-                />
-              </div>
-              <div>
-                
-                <h3>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
-                  >
-                   Podras modificar toda su informacion
-                  </a>
-                </h3>
-                <p className="text-base text-body-color dark:text-dark-6">
-                  podras editar todos los toda la informacion de contacto
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section className="">
-      <Answer></Answer>
-      <Contacto></Contacto>
-    </section>
-    
-      
+      <section className="">
+        <Answer></Answer>
+        <Contacto></Contacto>
+      </section>
     </>
   );
 };
